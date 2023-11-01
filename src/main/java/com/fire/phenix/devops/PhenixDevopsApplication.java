601f7d6bb512b38fe2c2ee604d67ef088e8b643c
fire-phenix <703347383@qq.com>
2023-11-02 00:23:36 +0800

    init: 初始化项目依赖

---
+++ src/main/java/com/fire/phenix/devops/PhenixDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.fire.phenix.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhenixDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhenixDevopsApplication.class, args);
+    }
+
+}
